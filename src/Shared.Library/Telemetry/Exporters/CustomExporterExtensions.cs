5e9a88152a6a26b4f14b98b17d368b19ea84eb4d
serkutYILDIRIM <serkutyldrm@gmail.com>
2025-10-08 23:52:13 +0300

    code style fixed

--- src/Shared.Library/Telemetry/Exporters/CustomExporterExtensions.cs
+++ src/Shared.Library/Telemetry/Exporters/CustomExporterExtensions.cs
@@ -1,7 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OpenTelemetry.Trace;
 using OpenTelemetry;
+using OpenTelemetry.Trace;
 
 namespace Shared.Library.Telemetry.Exporters;
 
