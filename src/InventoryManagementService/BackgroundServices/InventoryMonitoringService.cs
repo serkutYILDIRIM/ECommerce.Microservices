07599e7460b8643cbb1565a261ec9d817dcc3a6a
serkutYILDIRIM <serkutyldrm@gmail.com>
2025-11-11 23:35:19 +0300

    code fixs

--- src/InventoryManagementService/BackgroundServices/InventoryMonitoringService.cs
+++ src/InventoryManagementService/BackgroundServices/InventoryMonitoringService.cs
@@ -13,6 +13,7 @@ namespace InventoryManagementService.BackgroundServices;
 /// Background service that periodically monitors inventory levels
 /// and triggers alerts for low stock items
 /// </summary>
+/// 
 public class InventoryMonitoringService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
